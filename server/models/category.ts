0b202882cd83a8a800aeb6e43743b54091324ac9
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2022-06-27 16:13:16 +0530

    complete category api

--- server/models/category.ts
+++ server/models/category.ts
@@ -1,6 +1,6 @@
-import {Schema, model, Document} from 'mongoose';
+import {Schema, model, CallbackWithoutResultAndOptionalError} from 'mongoose';
 
-export interface ICategory extends Document {
+export interface ICategory {
 	name: string;
 	icon: string;
 }
@@ -9,5 +9,14 @@ const CategorySchema = new Schema<ICategory>({
 	name: {type: String, required: true, unique: true, maxlength: 100},
 	icon: {type: String, required: true, maxlength: 64},
 });
+
+function transform(next: CallbackWithoutResultAndOptionalError) {
+	this.name = this.name.toUpperCase();
+	next();
+}
+
+CategorySchema.pre('save', transform);
+
+CategorySchema.pre('updateOne', transform);
 
 export const Category = model('categories', CategorySchema);
--- server/routes/category.ts
+++ server/routes/category.ts
@@ -1,28 +1,79 @@
 import {Router} from 'express';
-import { Auth } from '../middlewares';
-import { Category } from '../models';
+import {Auth} from '../middlewares';
+import {Category, ICategory} from '../models';
 
 const router = Router();
 
 router
-  .route('/')
-  .get(async (req, res) => {
+	.route('/')
+	.get(async (req, res) => {
+		try {
+			const categories = await Category.find();
+			return res.status(200).json({categories});
+		} catch (err) {
+			console.log(err);
+			return res.sendStatus(500);
+		}
+	})
+	.post(Auth.isAuthenticated, Auth.isAuthorised, async (req, res) => {
+		try {
+			const body = req.body as ICategory;
+			if (!body.name || !body.icon)
+				return res.status(400).json({msg: 'Invalid Data!'});
 
-  })
-  .post(Auth.isAuthenticated, Auth.isAuthorised, async (req, res) => {
-
-  })
+			const category = new Category({
+				name: body.name,
+				icon: body.icon,
+			});
+			await category.save();
+			return res.sendStatus(201);
+		} catch (err) {
+			console.log(err);
+			return res.sendStatus(500);
+		}
+	});
 
 router
-  .route('/:id')
-  .get(async (req, res) => {
+	.route('/:id')
+	.get(async (req, res) => {
+		try {
+			const id = req.params.id;
+			const category = await Category.findById(id);
+			return res.status(200).json({category});
+		} catch (err) {
+			console.log(err);
+			return res.sendStatus(500);
+		}
+	})
+	.put(Auth.isAuthenticated, Auth.isAuthorised, async (req, res) => {
+		try {
+			let {
+				body,
+				params: {id},
+			} = req;
+			if (!body.name && !body.icon)
+				return res.status(400).json({msg: 'Invalid Data!'});
 
-  })
-  .put(Auth.isAuthenticated, Auth.isAuthorised, async (req, res) => {
+			const updatedCategory: Partial<ICategory> = {};
+			if (body.name) updatedCategory.name = body.name;
+			if (body.icon) updatedCategory.icon = body.icon;
 
-  })
-  .delete(Auth.isAuthenticated, Auth.isAuthorised, async (req, res) => {
-
-  })
+			await Category.findByIdAndUpdate(id, updatedCategory);
+			return res.status(200).json({msg: 'Category Updated!'});
+		} catch (err) {
+			console.log(err);
+			return res.sendStatus(500);
+		}
+	})
+	.delete(Auth.isAuthenticated, Auth.isAuthorised, async (req, res) => {
+		try {
+			const id = req.params.id;
+			await Category.findByIdAndDelete(id);
+			return res.status(200).json({msg: 'Category Deleted!'});
+		} catch (err) {
+			console.log(err);
+			return res.sendStatus(500);
+		}
+	});
 
 export {router as categoryRouter};
